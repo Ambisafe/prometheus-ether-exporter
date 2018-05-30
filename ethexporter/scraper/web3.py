939926360c1e16f1b19b47a771d8731fc278128c
Denis Mishchishin <dennz78@gmail.com>
2018-05-30 14:05:50 +0300

    web3 intercation refactor

--- ethexporter/scraper/web3.py
+++ ethexporter/scraper/web3.py
@@ -1,52 +1,92 @@
-from collections import defaultdict
+from functools import lru_cache
 import logging
-from aioethereum import create_ethereum_client
+from aioethereum import AsyncIOHTTPClient
+from aioethereum.errors import BadJsonError, BadResponseError
+from aiohttp.client import ClientSession
+import asyncio
+import async_timeout
+import ujson
+from urllib.parse import urlparse
+
+
 
 _log = logging.getLogger(__name__)
+
+class AsyncioWEB3RPCClient(AsyncIOHTTPClient):
+
+    async def rpc_call(self, method, params=None, id_=None):
+        params = params or []
+        data = {
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params,
+            'id': id_ or self._id,
+        }
+        async with ClientSession(loop=self._loop) as client:
+            async with async_timeout.timeout(self._timeout, loop=self._loop):
+                response =  await client.post(
+                    url=self._endpoint,
+                    data=ujson.dumps(data),
+                    headers={'Content-Type': 'application/json'}
+                )
+                _log.debug('sent', extra={'response':response, 'data': data})
+                if response.status != 200:
+                    raise BadStatusError(response.status)
+
+                if id_ is None:
+                    self._id += 1
+                try:
+                    data = await response.json(loads=ujson.loads)
+                except ValueError:
+                    raise BadJsonError('Invalid jsonrpc response')
+                if 'result' not in data and 'error' in data:
+                    raise BadResponseError( data['error']['message'],
+                                            data['error']['code'])
+                elif 'result' not in data:
+                    raise KeyError('result')
+                else:
+                    return data['result']
+
+
+async def create_ethereum_client(uri, timeout=60, *, loop=None):
+    """Create client to ethereum node based on schema.
+
+    :param uri: Host on ethereum node
+    :type uri: str
+
+    :param timeout: An optional total time of timeout call
+    :type timeout: int
+
+    :param loop: An optional *event loop* instance
+                 (uses :func:`asyncio.get_event_loop` if not specified).
+    :type loop: :ref:`EventLoop<asyncio-event-loop>`
+
+    :return: :class:`BaseAsyncIOClient` instance.
+    """
+    if loop is None:
+        loop = asyncio.get_event_loop()
+
+    presult = urlparse(uri)
+    if presult.scheme in ('ipc', 'unix'):
+        reader, writer = await asyncio.open_unix_connection(presult.path,
+                                                                 loop=loop)
+        return AsyncIOIPCClient(reader, writer, uri, timeout, loop=loop)
+    elif presult.scheme in ('http', 'https'):
+        tls = presult.scheme[-1] == 's'
+        netloc = presult.netloc.split(':')
+        host = netloc.pop(0)
+        port = netloc.pop(0) if netloc else (443 if tls else 80)
+        return AsyncioWEB3RPCClient(host, port, tls, timeout, loop=loop)
+    else:
+        raise RuntimeError('This scheme does not supported.')
+
+
 async def nodes(context):
     from ethexporter import LabeledValue as LV
-    for node, url in (await context['urls']).items():
-        yield LV(await create_ethereum_client(url), {'node': node, 'url': url})
+    async for nodemap in context['urls']:
+        for node, url in nodemap.items():
+
+            yield LV(await create_ethereum_client(url), {'node': node, 'url': url})
 
 
-async def eth_lastblock_number(context):
-    from ethexporter import LabeledValue as LV
-    for node in await context['nodes']:
-        try:
-            yield LV(await node.value.eth_blockNumber(), node.labels)
-        except ConnectionError as e:
-            _log.exception(e)
 
-
-async def lastblock(context):
-    from ethexporter import LabeledValue as LV
-    nodes = await context['nodes']
-    lastblocks = await context['eth_lastblock_number']
-    node_lastblocks = defaultdict(list)
-    for node in nodes:
-        for lastblock in lastblocks:
-            if lastblock.labels['node'] == node.labels['node']:
-                node_lastblocks[node.labels['node']].extend([node.value, lastblock.value, lastblock.labels])
-
-    for name, (node, lastblock, labels) in node_lastblocks.items():
-        try:
-            yield LV(await node.eth_getBlockByNumber(lastblock), labels)
-        except ConnectionError as e:
-            _log.exception(e)
-
-async def eth_lastblock_size(context):
-    from ethexporter import LabeledValue as LV
-    for lastblock in await context['lastblock']:
-        yield LV(int(lastblock.value['size'], 0), lastblock.labels)
-
-
-async def eth_gas_limit(context):
-    from ethexporter import LabeledValue as LV
-    for lastblock in await context['lastblock']:
-        yield LV(int(lastblock.value['gasLimit'], 0), lastblock.labels)
-
-
-async def eth_gas_used(context):
-    from ethexporter import LabeledValue as LV
-    for lastblock in await context['lastblock']:
-        yield LV(int(lastblock.value['gasUsed'], 0), lastblock.labels)
